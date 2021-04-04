a95c8589ba41b82dbe09af3d38387db6f971133d
Abidzar16 <abidzarisprivate@gmail.com>
2021-04-04 12:47:49 +0000

    Update file server.js

--- headerparser-microservice/server.js
+++ headerparser-microservice/server.js
@@ -2,14 +2,13 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,13 +18,29 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string?", function(req, res) {
+  let date;
+  const numbersOnlyRegex = /^[0-9]*$/;
+  // set date variable based on param
+  if (req.params.date_string === undefined) {
+    date = new Date();
+  } else if (req.params.date_string.match(numbersOnlyRegex)) {
+    date = new Date(Number(req.params.date_string));
+  } else {
+    date = new Date(req.params.date_string);
+  };
+  // check and return JSON based on if param is valid
+  if (date instanceof Date && isFinite(date)) {
+    res.json({
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    });
+  } else {
+    res.json({
+      "error": "Invalid Date"
+    })
+  }
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
